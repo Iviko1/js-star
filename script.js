4e2ef241376860d473bf972d21ff842b42fc409f
iviko1 <iveri.kobakhidze.1@btu.edu.ge>
2021-10-03 11:46:25 +0400

    optimized

--- script.js
+++ script.js
@@ -1,59 +1,35 @@
+// Globals
+const BOXCOUNT = 24;
+const ROTATIONDECREMENT = 15;
+const BREAKWIDTH = 480;
+const BOXANIMATIONDURATION = 3000;
+const TEXTANIMATIONDURATION = 3000;
+// Code
 window.addEventListener("load", () => {
-  const CONTAINER = document.createElement("div");
-  const WELCOME = document.createElement("h1");
-  const BUTTON = document.createElement("button");
-  let animationHeight = window.innerWidth > 768 ? "4rem" : "2rem";
-  let HEIGHT = window.innerWidth > 768 ? "20rem" : "10rem";
-  WELCOME.innerHTML = "IveX";
-  WELCOME.classList.add("welcome");
-  CONTAINER.classList.add("container");
-  BUTTON.innerText = "Change Color";
-  document.body.appendChild(WELCOME);
-  document.body.appendChild(CONTAINER);
-  document.body.appendChild(BUTTON);
-  // Welcome Animation
-  WELCOME.animate(
+  const NAMETEXT = document.querySelector(".name");
+  const CONTAINER = document.querySelector(".box-container");
+  const BUTTON = document.querySelector(".btn");
+  let animationHeight = window.innerWidth > BREAKWIDTH ? "4rem" : "2rem";
+  let HEIGHT = window.innerWidth > BREAKWIDTH ? "20rem" : "10rem";
+  // Name Animation
+  NAMETEXT.animate(
     [
       { transform: "translateY(0)" },
       { transform: `translateY(1rem)` },
       { transform: "translateY(0)" },
     ],
     {
-      duration: 2000,
+      duration: TEXTANIMATIONDURATION,
       iterations: Infinity,
     }
   );
   // Box Animation
   let degrees = 360;
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < BOXCOUNT; i++) {
     const box = document.createElement("div");
     box.classList.add(`box`);
-    box.animate(
-      [
-        // KEYFRAMES
-        {
-          transform: `rotate(${0}deg)`,
-          borderRadius: "0%",
-          height: HEIGHT,
-        },
-        {
-          transform: `rotate(${degrees}deg)`,
-          borderRadius: "50%",
-          height: animationHeight,
-        },
-        {
-          transform: `rotate(${0}deg)`,
-          borderRadius: "0%",
-          height: HEIGHT,
-        },
-      ],
-      {
-        // TIMING
-        duration: 4000,
-        iterations: Infinity,
-      }
-    );
-    degrees -= 15;
+    animateBox(box, HEIGHT, animationHeight, BOXANIMATIONDURATION, degrees);
+    degrees -= ROTATIONDECREMENT;
     CONTAINER.appendChild(box);
   }
   // Random Color
@@ -61,48 +37,55 @@ window.addEventListener("load", () => {
   BUTTON.addEventListener("click", () => {
     const COLOR =
       "#" + (~~(Math.random() * 2 ** 24)).toString(16).padStart(6, 0);
-    WELCOME.style.color = COLOR;
+    NAMETEXT.style.color = COLOR;
     BOXES.forEach((box) => (box.style.borderColor = COLOR));
     BUTTON.style.color = COLOR;
     BUTTON.style.borderColor = COLOR;
     BUTTON.style.outlineColor = COLOR;
   });
-});
-
-// Responsiveness
-window.addEventListener("resize", () => {
-  animationHeight = window.innerWidth > 768 ? "4rem" : "2rem";
-  HEIGHT = window.innerWidth > 768 ? "20rem" : "10rem";
-
-  const BOXES = document.querySelectorAll(".box");
-  let degrees = 360;
-
-  BOXES.forEach((box) => {
-    box.animate(
-      [
-        // KEYFRAMES
-        {
-          transform: `rotate(${0}deg)`,
-          borderRadius: "0%",
-          height: HEIGHT,
-        },
-        {
-          transform: `rotate(${degrees}deg)`,
-          borderRadius: "50%",
-          height: animationHeight,
-        },
-        {
-          transform: `rotate(${0}deg)`,
-          borderRadius: "0%",
-          height: HEIGHT,
-        },
-      ],
-      {
-        // TIMING
-        duration: 4000,
-        iterations: Infinity,
-      }
-    );
-    degrees -= 15;
+  // Responsiveness
+  window.addEventListener("resize", () => {
+    animationHeight = window.innerWidth > BREAKWIDTH ? "4rem" : "2rem";
+    HEIGHT = window.innerWidth > BREAKWIDTH ? "20rem" : "10rem";
+    let degrees = 360;
+    BOXES.forEach((box) => {
+      animateBox(box, HEIGHT, animationHeight, BOXANIMATIONDURATION, degrees);
+      degrees -= ROTATIONDECREMENT;
+    });
   });
 });
+
+// Box Animation Function
+const animateBox = (
+  box,
+  boxHeight,
+  animationHeight,
+  animationDuration,
+  degrees
+) => {
+  box.animate(
+    [
+      // KEYFRAMES
+      {
+        transform: `rotate(0deg)`,
+        borderRadius: "0%",
+        height: boxHeight,
+      },
+      {
+        transform: `rotate(${degrees}deg)`,
+        borderRadius: "50%",
+        height: animationHeight,
+      },
+      {
+        transform: `rotate(360deg)`,
+        borderRadius: "0%",
+        height: boxHeight,
+      },
+    ],
+    {
+      // TIMING
+      duration: animationDuration,
+      iterations: Infinity,
+    }
+  );
+};
